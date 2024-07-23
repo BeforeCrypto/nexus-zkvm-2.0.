c2bce5bfab85d2a8db07dd99bfbc13c0468061cb
BeforeCrypto BC <cryptobeforelaunch@gmail.com>
2024-07-23 11:58:36 +0000

    nexus 2.0 proof

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/nexus-zkvm-2.0./nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspace/nexus-zkvm-2.0./nexus-project/src/main.rs
---
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/nexus-zkvm-2.0./nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspace/nexus-zkvm-2.0./nexus-project/src/main.rs
